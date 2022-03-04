9b7f38de38fd2ec0e0165501bf726b1f84d67ec5
sebaspv <poncegramming@gmail.com>
2022-03-04 13:10:29 -0600

    added license to header file

--- kernel.cuh
+++ kernel.cuh
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2022 Sebastian Ponce. All rights reserved.
+
+This work is licensed under the terms of the MIT license.  
+For a copy, see <https://opensource.org/licenses/MIT>.
+*/
+
+
 #pragma once
 #include <vector>
 
